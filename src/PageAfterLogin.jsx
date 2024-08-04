2c2af5990f152bd574b9562113ac3f63387e4bf7
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2024-08-04 21:58:09 +0300

    fix

--- src/PageAfterLogin.jsx
+++ src/PageAfterLogin.jsx
@@ -1,7 +1,7 @@
 export default function PageAfterLogin({ handleExit, user }) {
   return (
     <>
-      <h2>{user.name}, Вы успешно авторизованы</h2>
+      <h2>{user.name}, Вы успешно авторизованы!</h2>
       <button onClick={handleExit}>
         Выйти
       </button>
